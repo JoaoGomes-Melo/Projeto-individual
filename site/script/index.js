1771d243a9ef4f7d9cae8746b78f0b3f83371c1e
JoaoGomes-Melo <joao.gmelo@sptech.school>
2025-11-15 20:15:30 -0300

    Subindo novos atulizações e arquivos

---
+++ site/script/index.js
@@ -0,0 +1,7 @@
+// vetor para troca de imagens dos personagens 
+var listaPersonagens = ["img/scooby.png","img/salsicha.png","img/fred.png","img/daphne.png","img/velma.png"];
+
+
+function proximo(){
+    var qtdPerso = listaPersonagens.length
+}
