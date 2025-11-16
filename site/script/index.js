3c259beb985638e8f78b95f47a698fc72c6016ed
JoaoGomes-Melo <joao.gmelo@sptech.school>
2025-11-16 11:41:05 -0300

    subindo pagina de login e cadastro

--- site/script/index.js
+++ site/script/index.js
@@ -1,7 +1,25 @@
-// vetor para troca de imagens dos personagens 
-var listaPersonagens = ["img/scooby.png","img/salsicha.png","img/fred.png","img/daphne.png","img/velma.png"];
+//troca de imagens dos personagens 
+
+    var listaPerso = ['perso-scooby', 'perso-salsicha', 'perso-fred', 'perso-daphne', 'perso-velma']
+
+    function proximo() {
+
+        for (var i = 0; i < listaPerso.length; i++) {
 
 
-function proximo(){
-    var qtdPerso = listaPersonagens.length
-}
+            var atual = listaPerso[i]
+            var divAtual = document.getElementById(atual)
+            var divComputada = window.getComputedStyle(divAtual)
+
+            if (divComputada.display === 'flex') {
+                divAtual.style.display = 'none'
+                var proximo = listaPerso[i + 1]
+                var px = document.getElementById(proximo)
+                px.style.display = 'flex'
+
+
+                i = listaPerso.length
+
+            }
+        }
+    }
