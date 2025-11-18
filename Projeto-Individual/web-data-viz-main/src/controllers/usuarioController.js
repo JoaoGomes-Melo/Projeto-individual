15602cd2cf368b76f51e44a543160e890e1e3d10
JoaoGomes-Melo <joao.gmelo@sptech.school>
2025-11-17 23:41:49 -0300

    Subindo a funcionalidade de cadastro atravez da api

--- Projeto-Individual/web-data-viz-main/src/controllers/usuarioController.js
+++ Projeto-Individual/web-data-viz-main/src/controllers/usuarioController.js
@@ -54,23 +54,31 @@ function autenticar(req, res) {
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
+    var apelido = req.body.apelidoServer
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
-    var fkEmpresa = req.body.idEmpresaVincularServer;
+    var sexo = req.body.sexoServer
+    var idade = req.body.idadeServer
+
+
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
+    } else if (apelido == undefined) {
+        res.status(400).send("Seu apelido está undefined!");
     } else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
-    } else if (fkEmpresa == undefined) {
-        res.status(400).send("Sua empresa a vincular está undefined!");
+    } else if (sexo == undefined) {
+        res.status(400).send("Seu sexo está undefined!");
+    } else if (idade == undefined) {
+        res.status(400).send("Sua idade  está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha, fkEmpresa)
+        usuarioModel.cadastrar(nome, email,apelido,senha,sexo,idade)
             .then(
                 function (resultado) {
                     res.json(resultado);
