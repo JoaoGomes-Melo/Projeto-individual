f5d7ac90ea879ee81a645967a950d38acea5f43e
JoaoGomes-Melo <joao.gmelo@sptech.school>
2025-11-22 19:53:40 -0300

    Subindo a cruzadinha semi-finalizada

--- Projeto-Individual/web-data-viz-main/public/js/jogo1.js
+++ Projeto-Individual/web-data-viz-main/public/js/jogo1.js
@@ -1,2 +1,41 @@
-var listaRepostas = ['simple plan','yeti','toqui','sanduiche','paris']
+// vetor de respostas
+var respostas = [
+    "",
+    "simple plan",
+    "yeti",
+    "toquio",
+    "sanduiche",
+    "paris"
+];
 
+// vetor com as classes das letras
+var classes = [
+    "",
+    "Simple",
+    "yeti",
+    "toquio",
+    "Sanduiche",
+    "Paris"
+];
+var pontos = 0
+// função para mostrar as letras
+function mostrarLetras(nomeClasse) {
+    var letras = document.getElementsByClassName(nomeClasse);
+
+    for (var i = 0; i < letras.length; i++) {
+        letras[i].style.opacity = "1";
+        letras[i].style.color = "black";
+    }
+}
+
+function validar(campo, numeroPergunta) {
+    var respostaUsuario = campo.value.trim().toLowerCase();
+    var respostaCerta = respostas[numeroPergunta];
+
+    if (respostaUsuario === respostaCerta) {
+        pontos += 10
+        mostrarLetras(classes[numeroPergunta]);
+    } else {
+        alert("Resposta errada!");
+    }
+}
