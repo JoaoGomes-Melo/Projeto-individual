77af366b76b196c90ec83796c9cb52f7dee70db6
JoaoGomes-Melo <joao.gmelo@sptech.school>
2025-11-23 21:04:27 -0300

    Subindo o script do banco de dados

---
+++ Banco de dados/script.sql
@@ -0,0 +1,38 @@
+USE ScoobyDoo;
+
+CREATE TABLE usuario (
+idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45) NOT NULL,
+apelido  VARCHAR(45) NOT NULL UNIQUE,
+email VARCHAR(255) NOT NULL UNIQUE,
+sexo VARCHAR(45) NOT NULL,
+dtNasc DATE NOT NULL,
+senha VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE jogo (
+idJogo INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(45),
+tipo VARCHAR(45),
+CONSTRAINT chkTipo CHECK (tipo in ("cruzadinha"))
+);
+
+INSERT INTO jogo(nome,tipo) VALUES
+('What´s New Scooby-Doo','cruzadinha');
+
+CREATE TABLE placar(
+idPlacar INT AUTO_INCREMENT,
+fkJogo INT,
+fkUsuario INT,
+pontuacao INT,
+dtjogo DATETIME DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (idPlacar,fkJogo,fkUsuario),
+CONSTRAINT FkJogoPlacar FOREIGN KEY (fkJogo) REFERENCES jogo (idJogo),
+CONSTRAINT fkUsuarioPlacar FOREIGN KEY (fkUsuario) REFERENCES usuario (idUsuario)
+);
+show tables;
+
+insert into placar(idPlacar,fkjogo,fkusuario,pontuacao) values 
+(1,1,1,200);
+
+select * from placar;
